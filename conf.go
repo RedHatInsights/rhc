2e695fc7405a2e5db83b67ea8234a74bd230e592
Jiri Hnidek <jhnidek@redhat.com>
2024-11-06 15:17:47 +0100

    fix: Removed unused CreateTLSClientConfig function
    * This function is not needed anymore. This function has not been
      used since #149 was merged

--- conf.go
+++ conf.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"crypto/tls"
-	"crypto/x509"
-	"fmt"
-	"os"
-	"path/filepath"
-)
-
 const (
 	cliLogLevel  = "log-level"
 	cliCertFile  = "cert-file"
@@ -20,53 +12,6 @@ type Conf struct {
 	KeyFile  string
 	LogLevel string
 	CADir    string
-}
-
-// Create a TLSConfig using the rhsm certificates
-func (conf *Conf) CreateTLSClientConfig() (*tls.Config, error) {
-	var certData, keyData []byte
-	var err error
-	rootCAs := make([][]byte, 0)
-
-	KeyDir := conf.KeyFile
-	CertDir := conf.CertFile
-
-	certData, err = os.ReadFile(CertDir)
-	if err != nil {
-		return nil, fmt.Errorf("cannot read cert-file: %w", err)
-	}
-
-	keyData, err = os.ReadFile(KeyDir)
-	if err != nil {
-		return nil, fmt.Errorf("cannot read key-file: %w", err)
-	}
-	CAFiles, err := os.ReadDir(conf.CADir)
-	if err != nil {
-		return nil, fmt.Errorf("cannot read ca files: %w", err)
-	}
-	for _, file := range CAFiles {
-		fPath := filepath.Join(conf.CADir, file.Name())
-		data, err := os.ReadFile(fPath)
-		if err != nil {
-			return nil, fmt.Errorf("cannot read ca-file %s : %w", fPath, err)
-		}
-		rootCAs = append(rootCAs, data)
-	}
-
-	tlsConfig := &tls.Config{}
-	cert, err := tls.X509KeyPair(certData, keyData)
-	if err != nil {
-		return nil, fmt.Errorf("cannot create key pair: %w", err)
-	}
-	tlsConfig.Certificates = []tls.Certificate{cert}
-
-	// Create a pool with CAcerts from rhsm CA directory
-	pool := x509.NewCertPool()
-	for _, data := range rootCAs {
-		pool.AppendCertsFromPEM(data)
-	}
-
-	return tlsConfig, nil
 }
 
 var config = Conf{}
