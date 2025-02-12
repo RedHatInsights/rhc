9e3114112b0f41e916ee703e5a587daf2855596f
Jiri Hnidek <jhnidek@redhat.com>
2025-02-12 15:18:55 +0100

    fix: Return non-zero exit code for wrong usage of CLI options
    * Card ID: CCT-1155
    * When wrong combination of CLI option is used, then return non-zero
      exit code (64). Following wrong combinations are considered:
      * --username and --activation-key cannot be used together
      * --password and --activation-key cannot be used together
      * --activation-key cannot be used alone
    * Added set of constants for exit codes (copy pasted from
      sysexits.h
    * Use cli.Exit() for generating exit error and code. The
      code used in this case is 64 (ExitCodeUsage) that should
      be used, when e.g. wron combination of CLI options is used
    * TODO: modify all errors returned from BeforeAction() functions
      to use right code

--- connect_cmd.go
+++ connect_cmd.go
@@ -79,10 +79,27 @@ func beforeConnectAction(ctx *cli.Context) error {
 
 	if len(activationKeys) > 0 {
 		if username != "" {
-			return fmt.Errorf("--username and --activation-key can not be used together")
+			exitErr := cli.Exit(
+				"--username and --activation-key can not be used together",
+				ExitCodeUsage,
+			)
+			return exitErr
+
+		}
+		if password != "" {
+			exitErr := cli.Exit(
+				"--password and --activation-key can not be used together",
+				ExitCodeUsage,
+			)
+			return exitErr
+
 		}
 		if organization == "" {
-			return fmt.Errorf("--organization is required, when --activation-key is used")
+			exitErr := cli.Exit(
+				"--organization is required, when --activation-key is used",
+				ExitCodeUsage,
+			)
+			return exitErr
 		}
 	}
 
