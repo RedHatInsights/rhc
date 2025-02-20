803b75a7d63bbee55b375d238c2abbf34f8ab2c3
Jiri Hnidek <jhnidek@redhat.com>
2025-02-20 15:34:41 +0100

    test: Enable integration tests for wrong combination of CLI options.
    * Card ID: CCT-1155
    * Enable again two integration tests for wrong combination of
      CLI option

--- integration-tests/test_connect.py
+++ integration-tests/test_connect.py
@@ -78,26 +78,19 @@ def test_connect(external_candlepin, rhc, test_config, auth):
             {"organization": "candlepin.org", "activation-key": "xpto123"},
             None,
         ),
-        # invalid combination of parameters
-        pytest.param(
+        (  # invalid combination of parameters (username & activation-key)
             {
                 "username": "candlepin.username",
                 "activation-key": "candlepin.activation_keys",
             },
             None,
-            marks=pytest.mark.xfail(
-                reason="Unresolved Bug https://issues.redhat.com/browse/CCT-1155"
-            ),
         ),
-        pytest.param(
+        (  # invalid combination of parameters (password & activation-key)
             {
                 "activation-key": "candlepin.activation_keys",
                 "password": "candlepin.password",
             },
             None,
-            marks=pytest.mark.xfail(
-                reason="Unresolved Bug https://issues.redhat.com/browse/CCT-1155"
-            ),
         ),
     ],
 )
@@ -113,7 +106,7 @@ def test_connect_wrong_parameters(
     )
     command = ["connect"] + command_args
     result = rhc.run(*command, check=False)
-    assert result.returncode == 1
+    assert result.returncode != 0
     assert not yggdrasil_service_is_active()
 
 
