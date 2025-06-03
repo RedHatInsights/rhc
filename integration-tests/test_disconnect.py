cfa24b860f6e012495a62d9f7e3d673ae67cbbc5
Archana Pandey <arpandey@redhat.com>
2025-06-03 19:04:03 +0530

    feat(test): Added packit setup and integration tests for rhc 0.2 - Added integration tests for connect, disconnect, status commands - Added integration tests for man page - updated .packit.yml to run tests for rhel9 and rhel8

---
+++ integration-tests/test_disconnect.py
@@ -0,0 +1,87 @@
+"""
+This Python module contains integration tests for rhc.
+It uses pytest-client-tools Python module. More information about
+this module could be found: https://github.com/RedHatInsights/pytest-client-tools/
+"""
+
+import pytest
+import re
+
+from utils import rhcd_service_is_active
+
+
+def test_rhc_disconnect(external_candlepin, rhc, test_config):
+    """Verify that RHC disconnect command disconnects host from server
+    and deactivates rhcd service.
+    test_steps:
+        1- run rhc connect
+        2- run rhc disconnect
+    expected_output:
+        1- Assert exit code 0
+        2- Validate that these string are present in the stdout:
+            "Deactivated the Remote Host Configuration daemon",
+            "Disconnected from Red Hat Insights",
+            "Disconnected from Red Hat Subscription Management"
+    """
+    # Connect first to perform disconnect operation
+    rhc.connect(
+        username=test_config.get("candlepin.username"),
+        password=test_config.get("candlepin.password"),
+    )
+    assert rhc.is_registered
+    assert rhcd_service_is_active()
+    disconnect_result = rhc.run("disconnect", check=False)
+    assert disconnect_result.returncode == 0
+    assert not rhc.is_registered
+    assert not rhcd_service_is_active()
+    # copr builds have BrandName rhc and downstream builds have "Remote Host Configuration"
+    assert re.search(
+        r"(Deactivated the Remote Host Configuration daemon|Deactivated the rhc daemon)",
+        disconnect_result.stdout,
+    )
+    assert "Disconnected from Red Hat Insights" in disconnect_result.stdout
+    assert (
+        "Disconnected from Red Hat Subscription Management" in disconnect_result.stdout
+    )
+
+
+# @pytest.mark.skip(
+#   reason="Test cannot be run due to unresolved issue https://issues.redhat.com/browse/CCT-525"
+# )
+def test_disconnect_when_already_disconnected(rhc):
+    """Test RHC disconnect command when the host is already
+    disconnected from CRC
+    test_steps:
+      # rhc disconnect
+    expected_output:
+      1. validate that these string are present in the stdout:
+            "Deactivated the rhcd service",
+            "Cannot disconnect from Red Hat Subscription Management",
+            "insights  cannot disconnect from Red Hat Insights",
+            "rhsm      cannot disconnect from Red Hat Subscription Management: "
+            "warning: the system is already unregistered",
+    """
+    # one attempt to disconnect to ensure system is already disconnected
+    rhc.run("disconnect", check=False)
+    # second attempt to disconnect already disconnected system
+    disconnect_result = rhc.run("disconnect", check=False)
+    assert disconnect_result.returncode == 1
+    assert not rhc.is_registered
+    assert re.search(
+        r"(Deactivated the Remote Host Configuration daemon|Deactivated the rhc daemon)",
+        disconnect_result.stdout,
+    )
+    assert "Cannot disconnect from Red Hat Insights" in disconnect_result.stdout
+    assert (
+        "Cannot disconnect from Red Hat Subscription Management"
+        in disconnect_result.stdout
+    )
+    assert (
+        "rhsm      cannot disconnect from Red Hat Subscription Management:"
+        in disconnect_result.stdout
+    )
+    assert "warning: the system is already unregistered" in disconnect_result.stdout
+    assert (
+        "ERROR  insights  cannot disconnect from Red Hat Insights"
+        in disconnect_result.stdout
+    )
---
+++ integration-tests/test_status.py
@@ -0,0 +1,126 @@
+"""
+This Python module contains integration tests for rhc. It uses pytest-client-tools Python module.
+More information about this module could be found: https://github.com/RedHatInsights/pytest-client-tools/
+"""
+
+import pytest
+import json
+from pytest_client_tools import util
+from utils import rhcd_service_is_active
+import re
+
+
+def test_status_connected(external_candlepin, rhc, test_config):
+    """Test RHC Status command when the host is connected.
+    test_steps:
+        1- rhc connect
+        2- rhc status
+    expected_output:
+        1- Validate following strings in status command output
+            "Connected to Red Hat Subscription Management"
+            "Connected to Red Hat Insights"
+            "The rhcd service is active"
+    """
+    rhc.connect(
+        username=test_config.get("candlepin.username"),
+        password=test_config.get("candlepin.password"),
+    )
+    assert rhcd_service_is_active()
+    status_result = rhc.run("status", check=False)
+    assert status_result.returncode == 0
+    assert "Connected to Red Hat Subscription Management" in status_result.stdout
+    assert "Connected to Red Hat Insights" in status_result.stdout
+    # copr builds have BrandName rhc and downstream builds have "Remote Host Configuration"
+    assert re.search(
+        r"(The Remote Host Configuration daemon is active|The rhc daemon is active)",
+        status_result.stdout,
+    )
+
+
+def test_status_connected_format_json(external_candlepin, rhc, test_config):
+    """
+    Test 'rhc status --format json' command, when host is connected
+    test_steps:
+        1 - rhc connect
+        2 - rhc status
+    expected_output:
+        1 - Validate that output is valid JSON document
+        2 - Validate that JSON document contains expected data
+    """
+    rhc.connect(
+        username=test_config.get("candlepin.username"),
+        password=test_config.get("candlepin.password"),
+    )
+    status_result = rhc.run("status", "--format", "json", check=False)
+    assert status_result.returncode == 0
+    status_json = json.loads(status_result.stdout)
+    assert "hostname" in status_json
+    assert "rhsm_connected" in status_json
+    assert type(status_json["rhsm_connected"]) == bool
+    assert "insights_connected" in status_json
+    assert type(status_json["insights_connected"]) == bool
+    assert "rhcd_running" in status_json
+    assert type(status_json["rhcd_running"]) == bool
+
+
+def test_status_disconnected(rhc):
+    """Test RHC Status command when the host is disconnected.
+    Ref: https://issues.redhat.com/browse/CCT-525
+    test_steps:
+        1- rhc disconnect
+        2- rhc status
+    expected_output:
+        1- Validate following strings in status command output
+            "Not connected to Red Hat Subscription Management"
+            "Not connected to Red Hat Insights"
+            "The rhcd service is inactive"
+    """
+    # 'rhc disconnect' to ensure system is already disconnected
+    rhc.run("disconnect", check=False)
+    status_result = rhc.run("status", check=False)
+    assert status_result.returncode == 0
+    assert "Not connected to Red Hat Subscription Management" in status_result.stdout
+    assert "Not connected to Red Hat Insights" in status_result.stdout
+    # copr builds have BrandName rhc and downstream builds have "Remote Host Configuration"
+    assert re.search(
+        r"(The Remote Host Configuration daemon is inactive|The rhc daemon is inactive)",
+        status_result.stdout,
+    )
+
+
+def test_rhcd_service_restart(external_candlepin, rhc, test_config):
+    """
+    Test rhcd service can be restarted on connected and  not on disconnected system.
+    test_steps:
+        1- disconnect the system
+        2- restart rhcd service via systemctl
+        3- run rhc connect
+        4- restart rhcd service via systemctl
+    expected_results:
+        1- rhcd  should be restarted successfully on connected system
+    """
+
+    # 'rhc disconnect' to ensure system is already disconnected
+    rhc.run("disconnect", check=False)
+    util.logged_run("systemctl status rhcd --no-pager".split())
+    try:
+
+        util.logged_run("systemctl restart rhcd".split())
+        assert not rhcd_service_is_active()
+    except AssertionError as exc:
+        # for debugging lets check current state of rhcd service
+        util.logged_run("systemctl status rhcd --no-pager".split())
+        raise exc
+
+    # test rhcd service restart on a connected system
+    rhc.connect(
+        username=test_config.get("candlepin.username"),
+        password=test_config.get("candlepin.password"),
+    )
+    assert rhc.is_registered
+    try:
+        util.logged_run("systemctl restart rhcd".split())
+        assert rhcd_service_is_active()
+    except AssertionError as exc:
+        util.logged_run("systemctl status rhcd --no-pager".split())
+        raise exc
