49413370a766a53ffda60dc6ef0d33c44fc68c3e
Jiri Hnidek <jhnidek@redhat.com>
2025-07-18 22:48:44 +0200

    feat: Implemented enabling/disabling of collector
    * It is possible to enable and disable collector timer
      and start service using --now
    
    Signed-off-by: Jiri Hnidek <jhnidek@redhat.com>

--- collector_disable_cmd.go
+++ collector_disable_cmd.go
@@ -1,11 +1,48 @@
 package main
 
-import "github.com/urfave/cli/v2"
+import (
+	"context"
+	"fmt"
+	"github.com/redhatinsights/rhc/internal/systemd"
+	"github.com/urfave/cli/v2"
+	"path/filepath"
+)
 
 func beforeCollectorDisableAction(ctx *cli.Context) error {
-	return checkForUnknownArgs(ctx)
+	err := setupFormatOption(ctx)
+	if err != nil {
+		return err
+	}
+
+	if ctx.Args().Len() != 1 {
+		return fmt.Errorf("error: expected 1 argument of collector name, got %d", ctx.Args().Len())
+	}
+	return nil
 }
 
+// collectorDisableAction tries to disable collector timer
 func collectorDisableAction(ctx *cli.Context) (err error) {
+	collectorId := ctx.Args().First()
+
+	fileName := collectorId + ".toml"
+	collectorConfigfilePath := filepath.Join(collectorDirName, fileName)
+
+	collectorConfig, err := readCollectorConfig(collectorConfigfilePath)
+	if err != nil {
+		return cli.Exit(fmt.Sprintf("failed to read collector configuration file %s: %v", fileName, err), 1)
+	}
+
+	collectorTimer := collectorConfig.Systemd.Timer
+
+	conn, err := systemd.NewConnectionContext(context.Background(), systemd.ConnectionTypeSystem)
+	if err != nil {
+		return fmt.Errorf("cannot connect to systemd: %v", err)
+	}
+	defer conn.Close()
+
+	if err := conn.DisableUnit(collectorTimer, true, false); err != nil {
+		return fmt.Errorf("cannot enable timer %s: %v", collectorTimer, err)
+	}
+
 	return nil
 }
--- collector_enable_cmd.go
+++ collector_enable_cmd.go
@@ -1,11 +1,56 @@
 package main
 
-import "github.com/urfave/cli/v2"
+import (
+	"context"
+	"fmt"
+	"github.com/redhatinsights/rhc/internal/systemd"
+	"github.com/urfave/cli/v2"
+	"path/filepath"
+)
 
 func beforeCollectorEnableAction(ctx *cli.Context) error {
-	return checkForUnknownArgs(ctx)
+	err := setupFormatOption(ctx)
+	if err != nil {
+		return err
+	}
+
+	if ctx.Args().Len() != 1 {
+		return fmt.Errorf("error: expected 1 argument of collector name, got %d", ctx.Args().Len())
+	}
+	return nil
 }
 
+// collectorEnableAction tries to enable given collector timer and start
 func collectorEnableAction(ctx *cli.Context) (err error) {
+	collectorId := ctx.Args().First()
+	startNow := ctx.Bool("now")
+
+	fileName := collectorId + ".toml"
+	collectorConfigfilePath := filepath.Join(collectorDirName, fileName)
+
+	collectorConfig, err := readCollectorConfig(collectorConfigfilePath)
+	if err != nil {
+		return cli.Exit(fmt.Sprintf("failed to read collector configuration file %s: %v", fileName, err), 1)
+	}
+
+	collectorTimer := collectorConfig.Systemd.Timer
+	collectorService := collectorConfig.Systemd.Service
+
+	conn, err := systemd.NewConnectionContext(context.Background(), systemd.ConnectionTypeSystem)
+	if err != nil {
+		return fmt.Errorf("cannot connect to systemd: %v", err)
+	}
+	defer conn.Close()
+
+	if err := conn.EnableUnit(collectorTimer, true, false); err != nil {
+		return fmt.Errorf("cannot enable timer %s: %v", collectorTimer, err)
+	}
+
+	if startNow {
+		if err := conn.StartUnit(collectorService, false); err != nil {
+			return fmt.Errorf("cannot start service %s: %v", collectorService, err)
+		}
+	}
+
 	return nil
 }
