50fec598c4d7705989b26ba0634a306e6f431e00
Jiri Hnidek <jhnidek@redhat.com>
2025-07-18 15:54:02 +0200

    feat: POC: Use version command for getting version
    * When version command is defined in the collector configuration
      file, then this version command is used for gathering version
    * When it is not possible to run run version command, then the
      error is written to log and "-" is used on the place of version
      value
    
    Signed-off-by: Jiri Hnidek <jhnidek@redhat.com>

--- collector_info_cmd.go
+++ collector_info_cmd.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/urfave/cli/v2"
+	"log/slog"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 	"text/tabwriter"
 )
 
@@ -19,6 +23,30 @@ func beforeCollectorInfoAction(ctx *cli.Context) error {
 		return fmt.Errorf("error: expected 1 argument of collector name, got %d", ctx.Args().Len())
 	}
 	return nil
+}
+
+// runVersionCommand tries to run version command
+func runVersionCommand(collectorConfig *CollectorInfo) (*string, error) {
+	var outBuffer bytes.Buffer
+	if collectorConfig.Exec.VersionCommand == "" {
+		return nil, fmt.Errorf("no version command specified in %s", collectorConfig.configFilePath)
+	}
+	command := strings.Fields(collectorConfig.Exec.VersionCommand)
+	if len(command) == 0 {
+		return nil, fmt.Errorf("empty version command specified in %s", collectorConfig.configFilePath)
+	}
+	cmd := exec.Command(command[0], command[1:]...)
+	cmd.Stdout = &outBuffer
+	err := cmd.Run()
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to run collector '%s': %v", collectorConfig.Exec.VersionCommand, err)
+	}
+
+	stdOut := outBuffer.String()
+	version := strings.TrimSpace(stdOut)
+
+	return &version, nil
 }
 
 func collectorInfoAction(ctx *cli.Context) (err error) {
@@ -39,15 +67,25 @@ func collectorInfoAction(ctx *cli.Context) (err error) {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
 	if !uiSettings.isMachineReadable {
 		_, _ = fmt.Fprintf(w, "Name:\t%s\n", collectorConfig.Meta.Name)
+
+		// Try to get the collector version from version command
+		version, err := runVersionCommand(collectorConfig)
+		if err != nil {
+			slog.Error(fmt.Sprintf("failed to get collector version: %v", err))
+			_, _ = fmt.Fprintf(w, "Version:\t%s\n", notDefinedValue)
+		} else {
+			_, _ = fmt.Fprintf(w, "Version:\t%s\n", *version)
+		}
+
 		if collectorConfig.Meta.Feature != "" {
 			_, _ = fmt.Fprintf(w, "Feature:\t%s\n\n", collectorConfig.Meta.Feature)
 		} else {
 			_, _ = fmt.Fprintf(w, "Feature:\t%s\n\n", notDefinedValue)
 		}
 
-		// TODO: Get last run from systemd
+		// TODO: Get the last run from systemd
 		_, _ = fmt.Fprintf(w, "Last run:\t%s\n", notDefinedValue)
-		// TODO: Get next run from systemd
+		// TODO: Get the next run from systemd
 		_, _ = fmt.Fprintf(w, "Next run:\t%s\n\n", notDefinedValue)
 
 		_, _ = fmt.Fprintf(w, "Config:\t%s\n", filePath)
