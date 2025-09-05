0271928cfae9a4b4a565c3941ec941282652e57d
mhorky <mhorky@redhat.com>
2025-09-05 10:24:22 +0200

    chore: Move localization methods to own package
    * Card ID: CCT-1604
    
    To split out the rhc source code from project root to individual
    packages, it is necessary to move out locale in order to make it
    available everywhere, not just in the project root.

--- util.go
+++ util.go
@@ -78,15 +78,6 @@ func hasPriorityErrors(errorMessages map[string]LogMessage, level slog.Level) bo
 	return false
 }
 
-// getLocale tries to get current locale
-func getLocale() string {
-	// FIXME: Locale should be detected in more reliable way. We are going to support
-	//        localization in better way. Maybe we could use following go module
-	//        https://github.com/Xuanwo/go-locale. Maybe some other will be better.
-	locale := os.Getenv("LANG")
-	return locale
-}
-
 // checkForUnknownArgs returns an error if any unknown arguments are present.
 func checkForUnknownArgs(ctx *cli.Context) error {
 	if ctx.Args().Len() != 0 {
