d122c35824566d7d7775577e8f674652b454930f
James Molet <jmolet@redhat.com>
2025-10-15 15:26:01 -0400

    feat(ci): using tmt to install test dependencies for image mode, removed unused downstream PR job logic, skipping man page tests if they're not installed
    - Fixed syntax error with double 'fi' in test.sh

---
+++ systemtest/install-test-deps.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -ux
+
+source /etc/os-release
+
+# packages to install
+packages=(
+  "git-core"
+  "logrotate"
+  "man-db"
+  "podman"
+  "python3-pip"
+  "python3-pytest"
+  "rhc"
+)
+
+if [ "$ID" == "rhel" ]; then
+  packages+=(
+    "insights-client"
+  )
+fi
+
+get_image_name() {
+  if command -v jq > /dev/null; then
+    IMAGE=$(bootc status --format=json | jq -r '.status.booted.image.image.image')
+  else
+    IMAGE=$(bootc status --format=humanreadable | grep 'Booted image' | cut -d' ' -f 4)
+  fi
+  echo "$IMAGE"
+}
+
+is_bootc() {
+  command -v bootc > /dev/null &&
+    ! bootc status --format=humanreadable | grep -q 'System is not deployed via bootc'
+}
+
+if is_bootc; then
+  echo "info: running in bootc/image-mode, preparing new image"
+  IMAGE=$(get_image_name)
+  echo "info: current image is $IMAGE"
+
+  (podman pull $IMAGE || podman pull containers-storage:$IMAGE) || bootc image copy-to-storage --target $IMAGE
+  podman build --build-arg IMAGE=$IMAGE -t localhost/rhc-test:latest -f Containerfile systemtest/
+
+  echo "info: switching to new bootc image and rebooting"
+  bootc switch --transport containers-storage localhost/rhc-test:latest
+else
+  echo "info: installing dependencies"
+  dnf --setopt install_weak_deps=False install -y ${packages[@]}
+  echo "info: dependencies installed successfully"
+fi
