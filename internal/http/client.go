0d154bb8cfc7044663333a1d36add59e34af6cc2
pkoprda <pkoprda@redhat.com>
2026-03-09 16:36:51 +0100

    chore: Drop unused method in http module
    In `internal/http/client.go` there is unused method Get(), which can be
    safely dropped from the codebase.

--- internal/http/client.go
+++ internal/http/client.go
@@ -2,17 +2,12 @@ package httpapi
 
 import (
 	"crypto/tls"
-	"fmt"
 	"net/http"
 	"time"
 )
 
 // FIXME: Make uploadTimeout configurable
 const uploadTimeout = 60 * time.Second
-
-type Client struct {
-	client http.Client
-}
 
 // NewHTTPClient returns an HTTP client configured with TLS certificates for secure uploads.
 func NewHTTPClient(tlsConfig *tls.Config) *http.Client {
@@ -22,13 +17,4 @@ func NewHTTPClient(tlsConfig *tls.Config) *http.Client {
 		Timeout:   uploadTimeout,
 		Transport: transport,
 	}
-}
-
-func (c *Client) Get(url string) (*http.Response, error) {
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return nil, fmt.Errorf("cannot create HTTP request: %w", err)
-	}
-
-	return c.client.Do(req)
 }
