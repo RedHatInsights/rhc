e3b0473d838b5dbe88a3f202edcea99a9b7f2011
Jiri Hnidek <jhnidek@redhat.com>
2026-06-08 14:48:05 +0200

    feat: A new Varlink interface com.redhat.rhsm
    * Card ID: CCT-2348
    * Added a new interface file .varlink for `com.redhat.rhsm.testing`
      interface, new rhsmapi also contains generat.go and generated
      go file with generated Go code for given Varlink interface
    * The interface contains two methods:
      * `IsRegistered()`
      * `Ping()`
    * Introduced a new file `constants.go` containing constant with
      AppName.
    * It uses golang module: `github.com/jirihnidek/rhsm2`
    * Updated `go.mod` and `go.sum`
    
    Signed-off-by: Jiri Hnidek <jhnidek@redhat.com>

---
+++ cmd/rhc-server/rhsm.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/jirihnidek/rhsm2"
+)
+
+type ClientError struct {
+	Message string
+}
+
+func (e *ClientError) Error() string {
+	return e.Message
+}
+
+type ServerError struct {
+	Message string
+}
+
+func (e *ServerError) Error() string {
+	return e.Message
+}
+
+// GetStatus retrieves the current status of the Red Hat Subscription Management (RHSM) server.
+func GetStatus(ipcSender *string, locale *string, correlationID *string) (*rhsm2.RHSMStatus, error) {
+	appName := AppName
+	rhsmClient, err := rhsm2.GetRHSMClient(&appName, nil)
+	if err != nil {
+		return nil, &ClientError{Message: err.Error()}
+	}
+
+	// Create client information from provided parameters
+	clientInfo := rhsm2.RequestMetadata{IPCSender: ipcSender, Locale: locale, CorrelationId: correlationID}
+	status, err := rhsmClient.GetServerStatus(&clientInfo)
+	if err != nil {
+		return nil, &ServerError{Message: err.Error()}
+	}
+
+	return status, nil
+}
+
+// IsSystemRegistered checks if the system is registered with RHSM.
+// When it is not possible to retrieve the consumer UUID, it returns false.
+func IsSystemRegistered() (bool, error) {
+	appName := AppName
+	rhsmClient, err := rhsm2.GetRHSMClient(&appName, nil)
+	if err != nil {
+		return false, &ClientError{Message: err.Error()}
+	}
+
+	_, err = rhsmClient.GetConsumerUUID()
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
---
+++ varlink/rhsmapi/generate.go
@@ -0,0 +1,3 @@
+package rhsmapi
+
+//go:generate go run github.com/emersion/go-varlink/cmd/varlinkgen -i com.redhat.rhsm.testing.varlink
