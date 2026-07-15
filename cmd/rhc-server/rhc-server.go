2933edada00365186cc913829bf01fa74fb2f33b
Jiri Hnidek <jhnidek@redhat.com>
2026-07-15 16:52:27 +0200

    feat: Varlink API for com.redhat.rhsm.testing.content.release
    * Card ID: CCT-2376
    * Implemented Varlink API for release. Implemented the Varlink API
      as it was designed in design document. The interface
      com.redhat.rhsm.testing.content.release has 4 new methods for
      release management:
      * Download() ... download release from candlepin server
      * GetCurrentRelease() ... returns current release, when it is set
      * GetAvailableReleases() ... returns the list of available releases
      * SetRelease() ... set the release in the release file and sends the
          release to candlepin server
    
    Signed-off-by: Jiri Hnidek <jhnidek@redhat.com>

--- cmd/rhc-server/rhc-server.go
+++ cmd/rhc-server/rhc-server.go
@@ -5,10 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
+	"slices"
 
 	"github.com/jirihnidek/rhsm2"
 	"github.com/redhatinsights/rhc/internal/collector"
 	"github.com/redhatinsights/rhc/varlink/collectorapi"
+	"github.com/redhatinsights/rhc/varlink/releaseapi"
 	"github.com/redhatinsights/rhc/varlink/rhsmapi"
 )
 
@@ -114,4 +116,99 @@ func (b *RHSMBackend) IsRegistered(in *rhsmapi.IsRegisteredIn) (*rhsmapi.IsRegis
 		return &rhsmapi.IsRegisteredOut{Registered: false}, nil
 	}
 	return &rhsmapi.IsRegisteredOut{Registered: registered}, nil
+}
+
+// ComRedhatRhsmContentReleaseBackend implements the interface for the com.redhat.rhsm.content.release.varlink
+type ComRedhatRhsmContentReleaseBackend struct{}
+
+// NewComRedhatRhsmContentReleaseRHSMBackend creates a new ComRedhatRhsmContentReleaseBackend instance.
+func NewComRedhatRhsmContentReleaseRHSMBackend() *ComRedhatRhsmContentReleaseBackend {
+	return &ComRedhatRhsmContentReleaseBackend{}
+}
+
+// Download implements the interface for the com.redhat.rhsm.content.release.varlink. It
+func (c ComRedhatRhsmContentReleaseBackend) Download(in *releaseapi.DownloadIn) (*releaseapi.DownloadOut, error) {
+	registered, err := IsSystemRegistered()
+	if err != nil || !registered {
+		return nil, &releaseapi.SystemNotRegisteredError{}
+	}
+
+	var release *string
+	if in.Metadata != nil {
+		release, err = DownloadRelease(
+			in.Metadata.UserAgent,
+			in.Metadata.Locale,
+			in.Metadata.CorrelationId,
+		)
+	} else {
+		release, err = DownloadRelease(nil, nil, nil)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &releaseapi.DownloadOut{Release: *release}, nil
+}
+
+// GetAvailableReleases retrieves a list of available releases and sorts them alphabetically.
+func (c ComRedhatRhsmContentReleaseBackend) GetAvailableReleases(in *releaseapi.GetAvailableReleasesIn) (*releaseapi.GetAvailableReleasesOut, error) {
+	registered, err := IsSystemRegistered()
+	if err != nil || !registered {
+		return nil, &releaseapi.SystemNotRegisteredError{}
+	}
+
+	var releases map[string]struct{}
+	if in.Metadata != nil {
+		releases, err = GetAvailableReleases(in.Metadata.UserAgent, in.Metadata.Locale, in.Metadata.CorrelationId)
+	} else {
+		releases, err = GetAvailableReleases(nil, nil, nil)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert map of structure (workaround for absence of set in Go) to list of strings
+	// and sort it alphabetically.
+	var releaseList []string
+	for release := range releases {
+		releaseList = append(releaseList, release)
+	}
+	slices.Sort(releaseList)
+
+	return &releaseapi.GetAvailableReleasesOut{Releases: releaseList}, nil
+}
+
+func (c ComRedhatRhsmContentReleaseBackend) GetCurrentRelease(in *releaseapi.GetCurrentReleaseIn) (*releaseapi.GetCurrentReleaseOut, error) {
+	rhsmClient, err := rhsm2.GetRHSMClient(nil, nil)
+	if err != nil {
+		return nil, &ClientError{Message: err.Error()}
+	}
+
+	release, err := rhsmClient.GetDnfVarsRelease()
+	if err != nil {
+		return nil, &ServerError{Message: err.Error()}
+	}
+
+	// When release is empty string, return nil pointer
+	if release == "" {
+		return &releaseapi.GetCurrentReleaseOut{Release: nil}, nil
+	}
+
+	return &releaseapi.GetCurrentReleaseOut{Release: &release}, nil
+}
+
+func (c ComRedhatRhsmContentReleaseBackend) SetRelease(in *releaseapi.SetReleaseIn) (*releaseapi.SetReleaseOut, error) {
+	var err error
+	if in.Metadata != nil {
+		err = SetRelease(in.Release, in.Metadata.UserAgent, in.Metadata.Locale, in.Metadata.CorrelationId)
+	} else {
+		err = SetRelease(in.Release, nil, nil, nil)
+	}
+	if err != nil {
+		return nil, &ServerError{Message: err.Error()}
+	}
+
+	return &releaseapi.SetReleaseOut{Success: true}, nil
 }
