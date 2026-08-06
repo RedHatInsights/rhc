30a1c4d1ed7851258a8f81e7709251c3bc3eee21
Jiri Hnidek <jhnidek@redhat.com>
2026-08-06 17:55:18 +0200

    test: Try to print log of rhc-server, when step fails
    * This works only for the case, when the rhc-server is started
      manually using rhc-server > /var/log/rhc/rhc-server.log 2>&1
    * At the begining of scenario the number of lines in the log
      file is saved and when step of the scenario fail, then
      print the log since the scenario was started
    * TODO: Print logs, when rhc-server uses journald
    
    Signed-off-by: Jiri Hnidek <jhnidek@redhat.com>

--- features/environment.py
+++ features/environment.py
@@ -2,11 +2,14 @@
 This module contains environment setup and teardown functions for the test suite.
 """
 
+import os
+
 ENTITLEMENT_CERT_DIR = "/etc/pki/entitlement/"
 ENTITLEMENT_BACKUP_DIR_PREFIX = "entitlement-backup-"
 RELEASEVER_FILE = "/etc/dnf/vars/releasever"
 RHSM_HOST_CONFIG_DIR = "/etc/rhsm-host"
 ENTITLEMENT_HOST_CERT_DIR = "/etc/pki/entitlement-host"
+RHC_SERVER_LOG_FILE = "/var/log/rhc/rhc-server.log"
 
 
 def before_scenario(context, scenario) -> None:
@@ -16,7 +19,14 @@ def before_scenario(context, scenario) -> None:
     :param scenario: Scenario object
     :return: None
     """
-    pass
+
+    context.log_lines_before = 0
+    if os.path.exists(RHC_SERVER_LOG_FILE):
+        with open(RHC_SERVER_LOG_FILE, 'r') as f:
+            counter = 0
+            for _ in f:
+                counter += 1
+            context.log_lines_before = counter
 
 
 def after_scenario(context, scenario) -> None:
@@ -55,5 +65,14 @@ def after_step(context, step) -> None:
             print(f"context stdout: {context.cmd_stdout}")
         if hasattr(context, "cmd_stderr") and context.cmd_stderr:
             print(f"context stderr: {context.cmd_stderr}")
-        # TODO: Print logs of rhc-server since the scenario was started, which
-        #       could be useful for debugging
+        # Print logs of rhc-server since the scenario was started
+        if os.path.exists(RHC_SERVER_LOG_FILE):
+            with open(RHC_SERVER_LOG_FILE, 'r') as f:
+                counter = 0
+                print("rhc-server log lines since scenario start:")
+                for line in f:
+                    counter += 1
+                    if counter > context.log_lines_before:
+                        print(line, end='')
+        else:
+            print(f"rhc-server log file not found: {RHC_SERVER_LOG_FILE}")
